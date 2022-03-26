f28d46db73abf87b2f7f54a1934fe7ddd7a72ffb
tjava <mustyytee@gmail.com>
2022-03-26 11:12:52 +0100

    add swager docs

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,16 @@ import config from "config";
 import connect from "./utils/connect";
 import log from "./utils/logger";
 import createServer from "./utils/createServer";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 const port = config.get<number>("port");
 
+const swaggerJsDocs = YAML.load("./docs.yaml");
+
 const server = createServer();
+
+server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 server.listen(port, async () => {
     log.info(`server listenig on port ${port}`);
