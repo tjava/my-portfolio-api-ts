6963dc7d6331ba440a439f0f690b9f689a96649e
tjava <mustyytee@gmail.com>
2022-05-15 01:52:31 +0100

    add deployment workflows

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
-    port: 1313,
-    dbUri: "mongodb://localhost:27017/my-portfolio-api-ts",
+  port: 1313,
+  dbUri:
+    "mongodb+srv://E-C_api:Password4E-C_api@cluster0.zdylb.mongodb.net/my-portfolio-api-ts?retryWrites=true&w=majority",
 };
